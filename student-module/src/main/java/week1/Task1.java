c6a13874d5458d5b9015be5514fda842228e5374
sichkar.dima <sichkar.dima@gmail.com>
2019-02-07 23:44:44 +0200

    task 1 week 1 is finished

---
+++ student-module/src/main/java/week1/Task1.java
@@ -0,0 +1,20 @@
+package week1;
+
+/**
+ * 1. Есть текущий час от 0 до 24 (либо другое значение).
+ * Если время от 9 до 18, то выводим "Я на работе", в другом случае "Я отдыхаю")
+ * В случае введения неверных значений (больше 24 и меньше 0) выводим "Неверное число"
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        int time = Integer.parseInt(args[0]);
+
+        if (time < 0 || time >24){
+            System.out.println("Неверное число");
+        }else if (time > 9 && time < 18) {
+            System.out.println("Я на работе");
+        }else {
+            System.out.println("Я отдыхаю");
+        }
+    }
+}
