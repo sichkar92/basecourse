501354b3e396dd2a37f80317c19e66e358257bc0
sichkar.dima <sichkar.dima@gmail.com>
2019-03-01 01:18:08 +0200

    task 7 week 1 is finished

--- student-module/src/main/java/week1/Task7.java
+++ student-module/src/main/java/week1/Task7.java
@@ -9,6 +9,16 @@ public class Task7 {
         int month = Integer.parseInt(args[0]);
 
 
-//      Yours code...
+        if (month > 12 || month < 1){
+            System.out.println("Wrong input!");
+        } else if (month >= 3 && month <= 5) {
+            System.out.println("Spring");
+        } else if (month >= 6 && month <= 8) {
+            System.out.println("Summer");
+        }else if (month >= 9 && month <= 11){
+            System.out.println("Autumn");
+        }else {
+            System.out.println("Winter");
+        }
     }
 }
