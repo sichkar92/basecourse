485ece5cbe6ebbfd86d8bd942900a39384ea9c77
sichkar.dima <sichkar.dima@gmail.com>
2019-03-01 03:51:43 +0200

    task 9 week 1 is finished

--- student-module/src/main/java/week1/Task9.java
+++ student-module/src/main/java/week1/Task9.java
@@ -1,5 +1,5 @@
 package week1;
-
+import java.util.Scanner;
 /**
  * 9. Написать программу вычисления идеального веса пользователя.
  * Выдать рекомендации о необходимости поправиться либо похудеть.
@@ -7,7 +7,41 @@ package week1;
 public class Task9 {
     public static void main(String[] args) {
 
-        //      Yours code...
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter you name!");
+        String name = sc.nextLine();
+        System.out.println("Enter you gender! (M or W)");
+        String gender = sc.nextLine();
+        System.out.println("Enter you weight!");
+        double weight = sc.nextDouble();
+        System.out.println("Enter you height!");
+        double height = sc.nextDouble();
+
+        if (gender.equals("M")) {   // This operations for Man
+            double perfectWeightM = (height - 100) * 1.15;
+            double differWeightM = weight - perfectWeightM ; // with Different Weight we understand you fat or thin
+            if (differWeightM <= -3) {
+                System.out.println(name + ", your perfect weight is " + perfectWeightM + " You are THIN!!! You need to add " +
+                        -differWeightM+ " kilograms");
+            } else if (differWeightM >= 3) {
+                System.out.println(name + ", your perfect weight is " + perfectWeightM + " You are FAT!!! You need to lose " +
+                        differWeightM+ " kilograms");
+            } else {
+                System.out.println(name + ", you are beautiful, because you have perfect weight -  " + perfectWeightM);
+            }
+        } else {  // this operations for Women
+            double perfectWeightW = (height - 100) - (height - 150) / 2;
+            double differWeightW = weight - perfectWeightW ;
+            if (differWeightW <= -3) {
+                System.out.println(name + ", your perfect weight is " + perfectWeightW + " You are THIN!!! You need to add " +
+                        -differWeightW+ " kilograms");
+            } else if (differWeightW >= 3) {
+                System.out.println(name + ", your perfect weight is " + perfectWeightW + " You are FAT!!! You need to lose " +
+                        differWeightW+ " kilograms");
+            } else {
+                System.out.println(name + ", you are beautiful, because you have perfect weight -  " + perfectWeightW);
+            }
+        }
 
     }
 }
