5ce59fad093f482f0e1b82c1d245ba6ef1389686
sichkar.dima <sichkar.dima@gmail.com>
2019-03-04 01:27:29 +0200

    task 12 week 1 is finished

--- student-module/src/main/java/week1/Task12.java
+++ student-module/src/main/java/week1/Task12.java
@@ -8,7 +8,12 @@ public class Task12 {
     public static void main(String[] args) {
         int number = Integer.parseInt(args[0]);
 
-        //      Yours code...
+        if (number / 100000 +(number / 10000) % 10 + (number / 1000) % 10 == (number / 100) % 10 + (number / 10) % 10 +
+                + number % 10) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
 
     }
 }
