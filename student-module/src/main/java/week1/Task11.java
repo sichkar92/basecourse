eb3416a7be73c96cff5984ecd64bf25992cfc695
sichkar.dima <sichkar.dima@gmail.com>
2019-03-04 01:03:28 +0200

    task 11 week 1 is finished

--- student-module/src/main/java/week1/Task11.java
+++ student-module/src/main/java/week1/Task11.java
@@ -9,7 +9,17 @@ public class Task11 {
     public static void main(String[] args) {
         int number = Integer.parseInt(args[0]);
 
-        //      Yours code...
+       if (number < 1 || number > 99){
+           System.out.println("Incorrect number");
+       } else if (number >= 11 && number <= 14){
+           System.out.println(number+" копеек");
+       } else if (number % 10 == 1){
+           System.out.println(number+" копейка");
+       } else if (number % 10 >= 2 && number % 10 <= 4) {
+           System.out.println(number+" копейки");
+        }else {
+           System.out.println(number+" копеек");
+        }
 
     }
 }
