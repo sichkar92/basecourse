b3261f6790c94bdadc4f95ad0eab3893314772c2
sichkar.dima <sichkar.dima@gmail.com>
2019-03-04 02:05:25 +0200

    task 13 week 1 is finished

--- student-module/src/main/java/week1/Task13.java
+++ student-module/src/main/java/week1/Task13.java
@@ -12,7 +12,13 @@ public class Task13 {
         int circle = Integer.parseInt(args[0]);
         int square = Integer.parseInt(args[1]);
 
-        //      Yours code...
+        if (Math.sqrt(2*square) <= 2*Math.sqrt(circle/Math.PI)) {
+            System.out.println("Квадрат уместится в круг");
+        } else if (2*Math.sqrt(circle/Math.PI) <= Math.sqrt(square)) {
+            System.out.println("Круг уместится в квадрат");
+        } else {
+            System.out.println("Фигуры не вмещаются друг в друга");
+        }
 
     }
 }
